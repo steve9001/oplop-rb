a39ad9fc903242700211b2381c5e42fc34d97407
Steve Masterman <steve@vermonster.com>
2019-05-28 16:33:48 -0400

    wip

---
+++ lib/oplop/v2019.rb
@@ -0,0 +1,29 @@
+module Oplop
+  class Random
+    def initialize(seed)
+      @seed = seed % 2147483647
+      @seed += 2147483646 if @seed <= 0
+    end
+
+    def next
+      @seed = @seed * 16807 % 2147483647
+    end
+  end
+
+  class V2019
+    def initialize(label, master)
+      @label = label
+      @master = master
+    end
+
+    def raw_hash
+      # TODO: implement hash
+      @raw_hash ||= @label
+    end
+
+    def random
+      # TODO: convert raw hash to integer (bytes)
+      @random ||= Random.new(raw_hash)
+    end
+  end
+end
