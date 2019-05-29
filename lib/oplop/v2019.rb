f2c2da8bbfa8edc01c81e685192264bbb5ea189c
Steve Masterman <steve@vermonster.com>
2019-05-29 16:34:12 -0400

    wip

--- lib/oplop/v2019.rb
+++ lib/oplop/v2019.rb
@@ -1,4 +1,6 @@
 <<-DOC
+https://gist.github.com/steve9001/fae0d5f30e2ee735ef378d8458e5f699
+
 PBKDF2 password: master, salt: label, iterations: 10, length: 20, hash: sha256
 
 URL safe base64 without padding, RFC 4648
@@ -17,10 +19,18 @@ require 'base64'
 require 'openssl'
 
 module Oplop
+
+  LENGTH = 16
+
+  KDF_ITERATIONS = 10
+  KDF_LENGTH = 20
+  KDF_HASH = 'SHA256'
+
   UPPER = ('A'..'Z').to_a
   LOWER = ('a'..'z').to_a
   DIGIT = ('0'..'9').to_a
   SYMBOL = "!#$%&()*+,-./:;<=>?@[]^_{|}~".split("")
+
   class Random
     def initialize(seed)
       @seed = seed % 2147483647
@@ -39,12 +49,11 @@ module Oplop
     end
 
     def digest
-      #@digest ||= Base64.urlsafe_encode64(OpenSSL::KDF.pbkdf2_hmac(@master, salt: @label, iterations: 10, length: 20, hash: 'SHA256'), padding: false)
       @digest ||= \
         begin
-          key = OpenSSL::KDF.pbkdf2_hmac(@master, salt: @label, iterations: 10, length: 20, hash: 'SHA256')
+          key = OpenSSL::KDF.pbkdf2_hmac(@master, salt: @label, iterations: KDF_ITERATIONS, length: KDF_LENGTH, hash: KDF_HASH)
           encoded = Base64.urlsafe_encode64(key, padding: false)
-          encoded[0..15]
+          encoded[0..LENGTH - 1]
       end
     end
 
@@ -59,12 +68,12 @@ module Oplop
     def password
       positions = []
       while positions.length < 4 do
-        p = random.next % 16
+        p = random.next % LENGTH
         positions << p unless positions.include? p
       end
-      digest[positions[0]] = UPPER[random.next % 26]
-      digest[positions[1]] = LOWER[random.next % 26]
-      digest[positions[2]] = DIGIT[random.next % 10]
+      digest[positions[0]] = UPPER[random.next % UPPER.length]
+      digest[positions[1]] = LOWER[random.next % LOWER.length]
+      digest[positions[2]] = DIGIT[random.next % DIGIT.length]
       digest[positions[3]] = SYMBOL[random.next % SYMBOL.length]
       digest
     end
